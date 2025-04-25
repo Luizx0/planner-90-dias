75abb934b23bc84a9163ca1b3608949e2ef75d68
Luizx0 <Loue0982@gmail.com>
2025-04-25 13:57:33 -0300

    new commit

---
+++ C/day17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b;
+
+    if (a/2=0) {
+        printf("numero par")
+    }
+    else (a/2!=0){
+        printf("numero impar")
+    }
+
+
+
+}
