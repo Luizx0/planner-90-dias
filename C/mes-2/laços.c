e94cfe95a01df3409214a8d73160f14eb5f8c33c
Luizx0 <Loue0982@gmail.com>
2025-06-02 19:59:40 -0300

    create day 52

---
+++ C/mes-2/laços.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Dia 53 - Programa que recebe notas e calcula média até digitar -1 (usar while) 
+
+
+
+
+
+
+
+
+// Dia 52 - Criar um contador regressivo de 10 até 1 usando for 
+// int main(){
+// int a;
+// for (int i = 10 ; i > 0 ; i--) {
+//     printf("\n%d", i);
+// }
+// return 0;
+// }
+
+
+
+// Dia 51 - Imprimir os números pares de 0 a 20 usando do while 
+// int main() {
+//     int i = 0;
+
+//     printf("Numeros pares de 0 a 20:\n");
+
+//     do {
+//         if (i % 2 == 0) {
+//             printf("%d ", i);
+//         }
+//         i++;
+//     } while (i <= 20);
+
+//     printf("\n");
+
+//     return 0;
+// }
