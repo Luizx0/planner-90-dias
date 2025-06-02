7c9126909b55432d5ce0d4ce848467bafd21ac7d
Luizx0 <Loue0982@gmail.com>
2025-06-02 20:26:45 -0300

    create day 53

--- C/mes-2/laços.c
+++ C/mes-2/laços.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Dia 53 - Programa que recebe notas e calcula média até digitar -1 (usar while) 
+// Dia 53 - Programa que recebe notas e calcula média até digitar sair (usar while) 
+int main(){
+    float a, soma = 0;
+    int i = 0;
+    char sair[4];
 
+    while (1){
+        printf("\nEscreva seu numero (ou 'sair' para encerrar): ");
+        scanf("%s", &sair);
 
+        if (strcmp(sair, "sair") == 0 || strcmp(sair, "SAIR") == 0) {
+            break;
+        }
+
+        a = atof(sair);
+        soma += a;
+        i++;
+        printf("\nMedia parcial: %.2f", soma / i);
+    }
+    printf("\nMedia final: %.2f", soma / i);
+    return 0;
+}
 
 
 
@@ -11,12 +31,12 @@
 
 
 // Dia 52 - Criar um contador regressivo de 10 até 1 usando for 
-// int main(){
-// int a;
-// for (int i = 10 ; i > 0 ; i--) {
+//  int main(){
+//  int a;
+//  for (int i = 10 ; i > 0 ; i--) {
 //     printf("\n%d", i);
 // }
-// return 0;
+//  return 0;
 // }
 
 
